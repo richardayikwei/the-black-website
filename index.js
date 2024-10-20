0068032f34147c70bfe422369b504a5e836aad29
Richard Ayikwei <richardayikweibee@gmail.com>
2024-10-20 13:04:32 +0000

    further fixes to code

--- index.js
+++ index.js
@@ -46,7 +46,5 @@ $(document).ready(function () {
             d.getHours().toString().padStart(2, '0') + ":" +
             d.getMinutes().toString().padStart(2, '0') + ":" +
             d.getSeconds().toString().padStart(2, '0');
-
-        console.log(d.getSeconds());
     }
 });
